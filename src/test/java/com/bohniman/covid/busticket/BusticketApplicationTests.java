ec549f1d9896a40f6f0310232fc785c00a800d69
Bhaskor Jyoti Sarmah <bhaskor.sarmah@bohniman.com>
2020-04-27 18:39:26 +0530

    Initial Commit

---
+++ src/test/java/com/bohniman/covid/busticket/BusticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bohniman.covid.busticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
